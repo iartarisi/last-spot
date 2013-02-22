1aa3604ab40d873f4ab3b8d035b798aea5b0b378
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-02-22 23:46:25 +0100

    cache requests in redis

--- last-spot.rkt
+++ last-spot.rkt
@@ -1,6 +1,8 @@
 #!/usr/bin/env racket
 #lang racket
 (require net/url)
+(require (planet mordae/redis:1:1))
+
 
 (define LASTFM "http://ws.audioscrobbler.com/2.0/?method=library.getartists&format=json")
 (define APIKEY
@@ -9,7 +11,24 @@
 (define LASTFM-ARTISTS
   (string-append LASTFM "&api_key=" APIKEY "&limit=" RESULTS-LIMIT "&user="))
 
+(define RDB (redis-connect)) ;; TODO - host and port from config file
+
+
 (define (get-user-url user)
   (string->url (string-append LASTFM-ARTISTS user)))
 
-(print (read-line (get-pure-port (get-user-url "mapleoin"))))
+(define (fetch-user-artists user)
+  (read-line (get-pure-port (get-user-url user))))
+
+(define (get-user-artists user)
+  (let ([cached-artists (bytes->string/utf-8 (redis-query RDB "get" user))])
+    (if (string? cached-artists)
+        cached-artists
+        ; the result was probably #\null , but if it's not a string,
+        ; something bad happened so we have to recache it anyway
+        (let ([new-artists (fetch-user-artists user)])
+          (redis-query RDB "set" user new-artists)
+          new-artists))))
+            
+      
+(print (get-user-artists "mapleoin"))
